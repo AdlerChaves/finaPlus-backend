bfe53ab5dcb12a6e3848aa78a95fc6a9027b53b4
Adler Chaves <adlerchaves@gmail.com>
2025-08-14 14:36:28 -0300

    Configure STATIC_ROOT for production

--- core/settings.py
+++ core/settings.py
@@ -8,9 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y#-h7!($uq%^ha=554j#z44=)&vj^g0ymva4%=vg-ky7b2(u6w'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -122,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
