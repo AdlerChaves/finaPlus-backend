6abc4ae9b74d61e70bef016bae93cf197c9921b7
Adler Chaves <adlerchaves@gmail.com>
2025-08-18 10:17:15 -0300

    Configure ALLOWED_HOSTS for production

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'api.relda.com',
+    '107.21.69.214',
+]
 
 
 # Application definition
